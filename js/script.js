79972e3b095f1ffdc7b4f75f1e1d20e414717e4a
Shiller Dan <107235042+shillerdev@users.noreply.github.com>
2023-03-17 18:20:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 700,
+    autoplay: true,
+    autoplayspeed: 2000,
+    pauseonhover: true,
+    draggable: false,
+  });
+});
